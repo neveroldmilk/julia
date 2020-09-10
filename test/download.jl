70652d76c77acff7088ac8f18f7cb72dbbcddaab
Stefan Karpinski <stefan@karpinski.org>
2020-09-10 18:17:32 -0400

    replace Base.download with Downloads.download

--- test/download.jl
+++ test/download.jl
@@ -40,7 +40,7 @@ mktempdir() do temp_dir
 
     # Make sure that failed downloads do not leave files around
     missing_file = joinpath(temp_dir, "missing")
-    @test_throws ProcessFailedException download("https://httpbin.julialang.org/status/404", missing_file)
+    @test_throws ErrorException download("https://httpbin.julialang.org/status/404", missing_file)
     @test !isfile(missing_file)
 
     # Make sure we properly handle metachar ' on windows with ^ escaping
@@ -54,6 +54,6 @@ mktempdir() do temp_dir
 
     # Use a TEST-NET (192.0.2.0/24) address which shouldn't be bound
     invalid_host_file = joinpath(temp_dir, "invalid_host")
-    @test_throws ProcessFailedException download("http://192.0.2.1", invalid_host_file)
+    @test_throws ErrorException download("http://192.0.2.1", invalid_host_file)
     @test !isfile(invalid_host_file)
 end
